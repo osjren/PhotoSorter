7ad13431235c30e7f1893d8120b30731db13482a
Jinsong Ren <mailjren@gmail.com>
2012-04-20 01:40:37 +0100

    Updated documentation and license

--- PhotoSorter.py
+++ PhotoSorter.py
@@ -2,23 +2,26 @@
 """
 Copyright (C) 2005-2012 Jinsong Ren
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in 
-the Software without restriction, including without limitation the rights to 
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
-of the Software, and to permit persons to whom the Software is furnished to do 
-so, subject to the following conditions:
+For documentation of the project, please go to:
+http://osjren.github.com/PhotoSorter
 
-The above copyright notice and this permission notice shall be included in all 
-copies or substantial portions of the Software.
+For bugfixes, updates and support, please go to:
+https://github.com/osjren/PhotoSorter/issues
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
-SOFTWARE.
+This file is part of PhotoSorter.
+
+PhotoSorter is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+PhotoSorter is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with PhotoSorter.  If not, see <http://www.gnu.org/licenses/>.
 """
 #===============================================================================
 # Main functions:
@@ -91,6 +94,7 @@ SOFTWARE.
 #   2011.12.06 - Added: changed GetRealVideoDir to GetRealVideoDirFname to return the real fname as well.
 #   2011.12.07 - Added: can overwrite target encoded video files.
 #   2012.04.12 - v2.5.4. Added MIT license terms. Updated EXIF.py to v1.09
+#   2012.04.19 - Changed the license to GPL to support the free software movement
 #
 # Note:
 #   Use python v2.4. "os.walk()" is available only after v2.4.
